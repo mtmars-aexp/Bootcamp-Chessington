3b2d073e908d14f0f56b0171d58b065269813dbe
Morgan Mars <morgan.t.mars@aexp.com>
2019-09-25 11:37:10 +0100

    Added King piece movement

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -15,6 +15,84 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> movesArray = new ArrayList<>();
+
+        Coordinates to = new Coordinates(from.getRow() - 1, from.getCol()); //One space up.
+        if(isCoordinateValid(to, from, board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow() - 1, from.getCol() - 1); //One space up, one space left.
+        if(isCoordinateValid(to, from, board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow(), from.getCol() -1); //Zero spaces up, one space left.
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow() + 1, from.getCol() -1); //One space down, one space left.
+        System.out.println(to.getRow() + " " + to.getCol());
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow() + 1, from.getCol()); //One space down.
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow() + 1, from.getCol() + 1); //One space down, one space right.
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow(), from.getCol() + 1); //One space right.
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        to = new Coordinates(from.getRow() - 1, from.getCol() + 1); //One space up, one space right.
+        if(isCoordinateValid(to,from,board)){
+            movesArray.add(new Move(from, to));
+        }
+
+        return movesArray;
     }
+
+    public boolean isOnSouthernEdge(Coordinates from){
+        return from.getRow() == 7;
+    }
+
+    public boolean isOnNorthernEdge(Coordinates from){
+        return from.getRow() == 0;
+    }
+
+    public boolean isOnEasternEdge(Coordinates from){
+        return from.getCol() == 7;
+    }
+
+    public boolean isOnWesternEdge(Coordinates from){
+        return from.getCol() == 0;
+    }
+
+
+    public boolean isCoordinateValid(Coordinates to, Coordinates from, Board board){
+        if (to.getRow() >= 0 && to.getRow() <= 7 && to.getCol() >= 0 && to.getCol() <= 7){
+            if (board.get(to) == null){
+                return true;
+            } else {
+                if (board.get(to).getColour().equals(board.get(from).getColour())){
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+
 }
--- src/test/java/training/chessington/model/pieces/KingTest.java
+++ src/test/java/training/chessington/model/pieces/KingTest.java
@@ -1,7 +1,38 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class KingTest {
+
+    @Test
+    public void kingsCannotMoveOffLeftEdge() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece blackKing = new King(PlayerColour.BLACK);
+        Coordinates blackCoords = new Coordinates(3, 0);
+        board.placePiece(blackCoords, blackKing);
+
+        Piece whiteKing = new Pawn(PlayerColour.WHITE);
+        Coordinates whiteCoords = new Coordinates(4, 0);
+        board.placePiece(whiteCoords, whiteKing);
+
+        // Act
+        List<Move> blackMoves = blackKing.getAllowedMoves(blackCoords, board);
+        List<Move> whiteMoves = whiteKing.getAllowedMoves(whiteCoords, board);
+
+        // Assert
+        assertThat(blackMoves).doesNotContain(new Move(blackCoords, blackCoords.plus(0, -1)));
+        assertThat(whiteMoves).doesNotContain(new Move(whiteCoords, whiteCoords.plus(0,-1)));
+    }
+
 }
