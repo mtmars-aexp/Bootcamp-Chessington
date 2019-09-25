6d152e9d9b5a0e23fa58f374580b90254314a279
skupavsk <skupavsk@c02yh0wwjgh6.uk.aexp.com>
2019-09-25 12:05:33 +0100

    Refactored and added test of kingsCanCapture

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -17,81 +17,54 @@ public class King extends AbstractPiece {
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> movesArray = new ArrayList<>();
 
-        Coordinates to = new Coordinates(from.getRow() - 1, from.getCol()); //One space up.
-        if(isCoordinateValid(to, from, board)){
-            movesArray.add(new Move(from, to));
-        }
+        for (int i = -1; i <= 1; i++) {
 
-        to = new Coordinates(from.getRow() - 1, from.getCol() - 1); //One space up, one space left.
-        if(isCoordinateValid(to, from, board)){
-            movesArray.add(new Move(from, to));
-        }
+            for (int j = -1; j <= 1; j++) {
 
-        to = new Coordinates(from.getRow(), from.getCol() -1); //Zero spaces up, one space left.
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
-        }
+                if (i == 0 && j == 0) {
+                    continue;
+                } else {
+                    Coordinates to = new Coordinates(from.getRow() + i, from.getCol() + j); //One space up, one space left.
+                    if (isCoordinateValid(to, board)) {
+                        movesArray.add(new Move(from, to));
+                    }
+                }
 
-        to = new Coordinates(from.getRow() + 1, from.getCol() -1); //One space down, one space left.
-        System.out.println(to.getRow() + " " + to.getCol());
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
-        }
+            }
 
-        to = new Coordinates(from.getRow() + 1, from.getCol()); //One space down.
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
-        }
-
-        to = new Coordinates(from.getRow() + 1, from.getCol() + 1); //One space down, one space right.
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
-        }
-
-        to = new Coordinates(from.getRow(), from.getCol() + 1); //One space right.
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
-        }
-
-        to = new Coordinates(from.getRow() - 1, from.getCol() + 1); //One space up, one space right.
-        if(isCoordinateValid(to,from,board)){
-            movesArray.add(new Move(from, to));
         }
 
         return movesArray;
     }
 
-    public boolean isOnSouthernEdge(Coordinates from){
+    public boolean isOnSouthernEdge(Coordinates from) {
         return from.getRow() == 7;
     }
 
-    public boolean isOnNorthernEdge(Coordinates from){
+    public boolean isOnNorthernEdge(Coordinates from) {
         return from.getRow() == 0;
     }
 
-    public boolean isOnEasternEdge(Coordinates from){
+    public boolean isOnEasternEdge(Coordinates from) {
         return from.getCol() == 7;
     }
 
-    public boolean isOnWesternEdge(Coordinates from){
+    public boolean isOnWesternEdge(Coordinates from) {
         return from.getCol() == 0;
     }
 
 
-    public boolean isCoordinateValid(Coordinates to, Coordinates from, Board board){
-        if (to.getRow() >= 0 && to.getRow() <= 7 && to.getCol() >= 0 && to.getCol() <= 7){
-            if (board.get(to) == null){
+    public boolean isCoordinateValid(Coordinates to, Board board) {
+        if (to.getRow() >= 0 && to.getRow() <= 7 && to.getCol() >= 0 && to.getCol() <= 7) {
+
+            if (board.get(to) == null) {
                 return true;
-            } else {
-                if (board.get(to).getColour().equals(board.get(from).getColour())){
-                    return false;
-                } else {
-                    return true;
-                }
             }
-        } else {
-            return false;
+
+            return !board.get(to).getColour().equals(getColour());
+
         }
+        return false;
     }
 
 
--- src/test/java/training/chessington/model/pieces/KingTest.java
+++ src/test/java/training/chessington/model/pieces/KingTest.java
@@ -35,4 +35,40 @@ public class KingTest {
         assertThat(whiteMoves).doesNotContain(new Move(whiteCoords, whiteCoords.plus(0,-1)));
     }
 
+    @Test
+    public void kingsCanMoveInAnyDirection() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece whiteKing = new Pawn(PlayerColour.WHITE);
+        Coordinates whiteCoords = new Coordinates(4, 4);
+        board.placePiece(whiteCoords, whiteKing);
+
+        // Act
+        List<Move> whiteMoves = whiteKing.getAllowedMoves(whiteCoords, board);
+
+        // Assert
+        assertThat((whiteMoves).size() == 7);
+    }
+
+    @Test
+    public void kingsCanCapture() {
+        // Arrange
+        Board board = Board.empty();
+
+        Piece whiteKing = new King(PlayerColour.WHITE);
+        Coordinates whiteCoords = new Coordinates(4, 4);
+        board.placePiece(whiteCoords, whiteKing);
+
+        Piece blackKing = new King(PlayerColour.BLACK);
+        Coordinates blackCoords = new Coordinates(3, 4);
+        board.placePiece(blackCoords, blackKing);
+
+        // Act
+        List<Move> whiteMoves = whiteKing.getAllowedMoves(whiteCoords, board);
+
+        // Assert
+        assertThat(whiteMoves).contains(new Move(whiteCoords, blackCoords));
+    }
+
 }
